ffeeb5db1589c15e123d641f65c2d53912246c08
BadDust80131 <45376544+BadDust80131@users.noreply.github.com>
2024-08-02 19:31:10 -0700

    Added CSS to buttons, hooked up buttons to JS functions

--- main.js
+++ main.js
@@ -29,3 +29,63 @@ function operate(val1,val2,oper) {
         return Math.round(val1 / val2);
     }
 }
+
+function buttonLogic(number) {
+    console.log(number)
+}
+
+
+function inputs(){
+    const one = document.querySelector("#one")
+    one.addEventListener("click", () => {
+        buttonLogic(1)
+    })
+
+    const two = document.querySelector("#two")
+    two.addEventListener("click", () => {
+        buttonLogic(2)
+    })
+
+    const three = document.querySelector("#three")
+    three.addEventListener("click", () => {
+        buttonLogic(3)
+    })
+
+    const four = document.querySelector("#four")
+    four.addEventListener("click", () => {
+        buttonLogic(4)
+    })
+
+    const five = document.querySelector("#five")
+    five.addEventListener("click", () => {
+        buttonLogic(5)
+    })
+
+    const six = document.querySelector("#six")
+    six.addEventListener("click", () => {
+        buttonLogic(6)
+    })
+
+    const seven = document.querySelector("#seven")
+    seven.addEventListener("click", () => {
+        buttonLogic(7)
+    })
+
+    const eight = document.querySelector("#eight")
+    eight.addEventListener("click", () => {
+        buttonLogic(8)
+    })
+
+    const nine = document.querySelector("#nine")
+    nine.addEventListener("click", () => {
+        buttonLogic(9)
+    })
+
+    const zero = document.querySelector("#zero")
+    zero.addEventListener("click", () => {
+        buttonLogic(0)
+    })
+}
+
+
+inputs()
